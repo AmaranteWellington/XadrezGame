389b6be2e107eff0685c4cf18126ffeddb2c03f8
Amarante Silva <amaranthwfs@gmail.com>
2023-01-26 10:15:46 -0300

    Criando a classe posição

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Posicao p;
+        p = new Posicao(3, 4);
+        Console.WriteLine(" Posição " + p);
+        
+    }
+}
